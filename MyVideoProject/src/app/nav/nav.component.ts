0cfdcfb929e10cb121f52d3e2da0f1c77de83f16
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-12-04 16:50:49 +0100

    Add Logout

--- MyVideoProject/src/app/nav/nav.component.ts
+++ MyVideoProject/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
@@ -10,7 +11,11 @@ import { ModalService } from '../services/modal.service';
 })
 export class NavComponent implements OnInit {
 
-  constructor(public modal: ModalService, public auth: AuthService) {
+  constructor(
+    public modal: ModalService, 
+    public auth: AuthService,
+    private afAuth: AngularFireAuth
+    ) {
   
    }
 
@@ -21,6 +26,12 @@ export class NavComponent implements OnInit {
     $event.preventDefault();
 
     this.modal.toggleModal('auth');
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault();
+
+    await this.afAuth.signOut();
   }
 
 }
