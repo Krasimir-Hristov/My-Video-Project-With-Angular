bc7c38844606006efc46e0808a4d40f3fdd8a934
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-12-09 20:25:22 +0100

    Add copy link button

--- MyVideoProject/src/app/video/manage/manage.component.ts
+++ MyVideoProject/src/app/video/manage/manage.component.ts
@@ -85,4 +85,18 @@ export class ManageComponent implements OnInit {
     })
   }
 
+  async copyToClipboard($event: MouseEvent, docID: string | undefined) {
+    $event.preventDefault();
+
+    if(!docID) {
+      return;
+    }
+
+    const url = `${location.origin}/clip/${docID}`;
+
+    await navigator.clipboard.writeText(url);
+
+    alert('Link Copied!');
+  }
+
 }
